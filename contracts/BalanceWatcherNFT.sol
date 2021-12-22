0037c4b7f07e92a50f4c63718964bee68ca599e3
Stephan Cilliers <hnzkqujglqhhetkdvrpizyald@forwardmail.rocks>
2021-12-22 19:55:04 +0200

    Track arbitrary tokens WIP

--- contracts/BalanceWatcherNFT.sol
+++ contracts/BalanceWatcherNFT.sol
@@ -4,17 +4,18 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "./Base64.sol";
 import "./IERC20.sol";
 
 contract BalanceWatcherNFT is ERC721 {
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
-    address public klimaAddress;
+    using EnumerableSet for EnumerableSet.AddressSet;
 
-    constructor(address _klimaAddress) ERC721("Balance Watcher", "WATCH") {
-        klimaAddress = _klimaAddress;
-    }
+    Counters.Counter private _tokenIds;
+    mapping (uint256 => address[5]) public tokenAddresses;
+
+    constructor() ERC721("Balance Watcher", "WATCH") { }
 
     function mint(address _address) public returns (uint256)
     {
@@ -26,17 +27,40 @@ contract BalanceWatcherNFT is ERC721 {
         return newItemId;
     }
 
+    function trackToken(uint256 _tokenId, address _tokenAddress, uint8 _index) public {
+        // Require that user is token owner
+        require(ownerOf(_tokenId) == msg.sender, "Not owner of address");
+        // TODO: require that uni price > 0
+        // TODO: Add eth tracking
+
+        tokenAddresses[_tokenId][_index] = _tokenAddress;
+    }
+
     function tokenURI(uint256 tokenId) override public view returns (string memory) {
-        IERC20 tokenContract = IERC20(klimaAddress);
+        
         // TODO: Track arbitrary tokens, which the user asks it to track
-        string[4] memory parts;
-        parts[0] = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>.base { fill: white; font-family: sans-serif; font-size: 24px; }</style><rect width="100%" height="100%" fill="green" /><text x="10" y="180" class="base">';
-        parts[1] = tokenContract.symbol();
-        parts[2] = Strings.toString(tokenContract.balanceOf(ownerOf(tokenId)));
-        parts[3] = '</text></svg>';
+        string memory output = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 300 300"><defs><linearGradient id="bg-gradient" gradientTransform="rotate(45, 0.5, 0.5)"><stop class="stop1" offset="0%"/><stop class="stop2" offset="50%"/><stop class="stop3" offset="100%"/></linearGradient><style>.base { fill: black; font-family: sans-serif; font-size: 24px; vertical-align: text-top;}.item {font-size: 18px;}.sub {font-size: 12px;}.stop1 { stop-color: #EEE9B8; }.stop2 { stop-color: #C0EEB8; }.stop3 { stop-color: #B8E5E3; }</style></defs><rect width="100%" height="100%" fill="url(#bg-gradient)" />';
+        output = string(abi.encodePacked(output, '<text x="26" y="44" class="base">Wallet #', Strings.toString(tokenId), '</text>'));
         
-        string memory output = string(abi.encodePacked(parts[0], parts[1], parts[2], parts[3]));
-        
+        address[5] memory _tokenAddresses = tokenAddresses[tokenId];
+        for (uint256 i = 0; i < 5; i++) {
+            address _tokenAddress = _tokenAddresses[i];
+            bytes[3] memory parts;
+            if (_tokenAddress == address(0)) {
+                continue;
+            } 
+            IERC20 tokenContract = IERC20(_tokenAddress);
+            /*
+            <text x="26" y="80" class="base item">ETH</text>
+            <text x="155" y="80" class="base item">$40,000</text>
+            <text x="26" y="95" class="base sub">10.0000</text>    
+            */
+            parts[0] = abi.encodePacked('<text x="26" y="', Strings.toString(80+45*i), '" class="base item">', tokenContract.symbol(), '</text>');
+            parts[1] = abi.encodePacked('<text x="145" y="', Strings.toString(80+45*i), '" class="base item">', Strings.toString(tokenContract.balanceOf(ownerOf(tokenId))), '</text>');
+            parts[2] = abi.encodePacked('<text x="26" y="', Strings.toString(95+45*i), '" class="base sub">', Strings.toString(tokenContract.balanceOf(ownerOf(tokenId))), '</text>');
+            output = string(abi.encodePacked(output, parts[0], parts[1], parts[2]));
+        }
+        output = string(abi.encodePacked(output, '</text></svg>'));
         string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "Balance Watcher #', Strings.toString(tokenId), '", "description": "This NFT displays the KLIMA balance of the owner address", "image": "data:image/svg+xml;base64,', Base64.encode(bytes(output)), '"}'))));
         output = string(abi.encodePacked('data:application/json;base64,', json));
 
--- test/test.js
+++ test/test.js
@@ -7,39 +7,43 @@ describe("BalanceNFT Tests", function () {
   let account2
   let account3
   let account4
-  let testPaymentToken
+  let token1
+  let token2
   let balanceWatcherNFT
 
   beforeEach(async () => {
     [deployer, account1, account2, account3, account4] = await ethers.getSigners()
 
-    const TestPaymentToken = await ethers.getContractFactory('ERC20PresetMinterPauser')
-    testPaymentToken = await TestPaymentToken.deploy('KlimaDAO', 'KLIMA')
-    await testPaymentToken.deployed()
-    await testPaymentToken.mint(account1.address, 100000)
+    const Token1 = await ethers.getContractFactory('ERC20PresetMinterPauser')
+    token1 = await Token1.deploy('KlimaDAO', 'KLIMA')
+    await token1.deployed()
+    await token1.mint(account1.address, 100000)
+
+    // TODO: Test eth balance and second token balance
+    const Token2 = await ethers.getContractFactory('ERC20PresetMinterPauser')
+    token2 = await Token1.deploy('Token 2', 'TOKEN2')
+    await token2.deployed()
+    await token2.mint(account1.address, 100000)
 
     const BalanceWatcherNFT = await ethers.getContractFactory('BalanceWatcherNFT')
-    balanceWatcherNFT = await BalanceWatcherNFT.deploy(testPaymentToken.address)
+    balanceWatcherNFT = await BalanceWatcherNFT.deploy()
     await balanceWatcherNFT.deployed()
-    
   })
 
   it("Should mint tokens to address", async function () {
-    expect(await testPaymentToken.balanceOf(account1.address)).to.equal(100000)
+    expect(await token1.balanceOf(account1.address)).to.equal(100000)
   });
-
-  it("Should deploy contract with correct address", async function () {
-    expect(await balanceWatcherNFT.klimaAddress.call()).to.equal(testPaymentToken.address)
-  })
 
   it("Should produce the correct SVG data for balance", async function () {
     await balanceWatcherNFT.mint(account1.address)
+    await balanceWatcherNFT.connect(account1).trackToken(1, token1.address, 0)
     const tokenURI = await balanceWatcherNFT.tokenURI(1)
     const tokenURIDecoded = atob(tokenURI.split(",")[1])
     const decodedSvg = atob(JSON.parse(tokenURIDecoded).image.split(",")[1])
 
-    const tokenSymbol = await testPaymentToken.symbol()
-    const balance = await testPaymentToken.balanceOf(account1.address)
+    console.log(decodedSvg)
+    const tokenSymbol = await token1.symbol()
+    const balance = await token1.balanceOf(account1.address)
 
     expect(decodedSvg).to.contain(tokenSymbol)
     expect(decodedSvg).to.contain(balance)
