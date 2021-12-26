2618746d056332bddbc855b548c2ead2b1bb1fd0
lbowles <lspbowles@gmail.com>
2021-12-26 21:34:41 +0200

    Landing page UI added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
+import ConnectWallet from './components/ConnectWallet';
+import ConnectWalletInfo from './components/ConnectWalletInfo';
 import Demo from './components/demo';
 import Logo from './components/Logo';
+import NFTInfo from './components/NFTInfo';
 import tester from './components/tester';
 
 function App() {
@@ -9,6 +12,11 @@ function App() {
       <div className="App-header">
         <div className="container">
           <Logo/>
+          <ConnectWalletInfo amountMinted={742}/>
+          <ConnectWallet/>
+          <div className="break"></div>
+          <Demo/>
+          <NFTInfo/>
         </div>
       </div>
     </div>
