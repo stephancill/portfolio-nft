b95214ec3a4d01397836a177cbed398c21b7794b
lbowles <lspbowles@gmail.com>
2022-01-06 11:07:17 +0200

    search tokens wip

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Portfolio NFT
 
-Allows the minter to track their ERC-20 token balances and USD values in an on-chain SVG 
+Allows the minter to track their ERC-20 token balances and USD values in an on-chain SVG
 without relying on any third parties.
 
 <img src="example.svg" alt="drawing" width="200"/>
@@ -10,24 +10,25 @@ without relying on any third parties.
 ```
 yarn install
 ```
+
 ```
 npx hardhat test
 ```
 
 ### Generate frontend ABIs
+
 ```
 npx hardhat deploy --export-all ./client/src/contracts.json
-FORK=mainnet npx hardhat node --network hardhat
-FORK=mainnet npx hardhat deploy --export-all ./client/src/contracts.json --network localhost
-
 ```
 
 ## Testing
+
 ```
 npx hardhat test
 ```
 
 With external network forking:
+
 ```
 FORK=polygon npx hardhat node --network hardhat
 FORK=polygon npx hardhat <task> --network localhost
@@ -36,22 +37,28 @@ FORK=polygon npx hardhat <task> --network localhost
 ### Frontend debugging with local chain
 
 1. Start node
+
 ```
 FORK=mainnet npx hardhat node --network hardhat
 ```
+
 2. Export ABIs
+
 ```
 FORK=mainnet npx hardhat deploy --export ./client/src/contracts.json --network localhost
 ```
 
 ## Deployment
+
 ### Checklist
+
 - [ ] Use Flashbots RPC to avoid paying for failed transactions
 - [ ] Confirm WETH symbol in `deploy/00_portfolio_nft.js`
 - [ ] Set correct name and symbol in `deploy/00_portfolio_nft.js`
 - [ ] Remove `hardhat/console.sol` imports
 - [ ] Verify contract on Etherscan
 - [ ] Verify contract on Sourcify
+
 ```
 npx hardhat deploy --network <network>
 npx hardhat etherscan-verify --network <network>
--- client/src/components/PortfolioAddTokens.js
+++ client/src/components/PortfolioAddTokens.js
@@ -3,13 +3,14 @@ import "./portfolioSetup.css"
 
 const PortfolioAddTokens = ({walletConnected}) => {
   const [tokenList,setTokenList] = useState()
+  const [tokens,setTokens] = useState([{}])
 
 
   useEffect(async()=>{
     if (walletConnected) {
       getTokenList()
     }
-  },[walletConnected])
+  },[walletConnected],[tokens])
 
   const getTokenList = async () => {
     await setTokenList( await getJSON("https://tokens.coingecko.com/uniswap/all.json"))
@@ -24,17 +25,37 @@ const PortfolioAddTokens = ({walletConnected}) => {
   const searchToken = (value) => {
     const tokenLength = tokenList.tokens.length
     const valueLenth = value.length
+    let showTokens = [{}]
+    let a = 0
     for (let i = 0;i < tokenLength; i++){
-      if (tokenList.tokens[i].symbol.substring(0,valueLenth) === value) {
-        console.log(value+"sosiamds")
+      if (tokenList.tokens[i].symbol.substring(0,valueLenth).toUpperCase() === value.toUpperCase()) {
+        showTokens[a] = {symbol:tokenList.tokens[i].symbol.toUpperCase(),logo:tokenList.tokens[i].logoURI}
+        a=a+1
+        console.log(tokenList)
       }
     }
+    
+    setTokens(showTokens)
   }
 
   return (
     <div>
       <input placeholder="Search a name, address or symbol" onChange={e => searchToken(e.target.value)}></input>
         <div className="b2">
+        {tokens.map((token, i) => ( <>
+            <div className="row" key={i} id={i}>
+              <div>
+                <img className="tokenIcon" src={token.logo}></img>
+              </div>
+              <div style={{marginLeft:"15px"}}>
+                <h3 style={{margin:"0px",marginTop:"-2px"}}>{token.symbol}</h3>
+                <h4 style={{margin:"0px",textAlign:"left"}} >test</h4>
+              </div>
+              <div className="col">
+              </div>
+            </div>
+            </>
+          ))}
         </div>
     </div>
   )
